9f4c1ba80b55894d8de09fe62ad9827062cdac3f
Marcus Jun <mjun@calstate.edu>
2018-03-15 02:53:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,7 +6,7 @@ function init() {
   let index=0
 
   function onKeyDownHandler(e) {
-    const key = e.key//parseInt(e.detail || e.which)
+    const key = parseInt(KeyboardEvent.key)//e.detail || e.which)
 
     if (key === code[index]) {
       index++
